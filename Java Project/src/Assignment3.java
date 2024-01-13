1c627cf2b93dca6409b249b563c43c4bb3384ec5
mshereef03 <mshereef2003@gmail.com>
2024-01-13 23:40:46 +0100

    Assignment 3

---
+++ Java Project/src/Assignment3.java
@@ -0,0 +1,134 @@
+import java.util.*;
+
+public class Assignment3 {
+
+    /*Given an undirected graph, explain how you can determine whether it is a tree or not. What would be the running time?
+
+    For it to be a tree it should be connected and not contain any cycles. To check for both of these properties we could do a DFS search, if at any point
+    a node has a connection to another node that has already been visited then it contains a cycle and is not a tree. If at the end no cycles were detected
+    and all nodes were visited then it's a tree, if some nodes were unvisited then the graph is disconnected and is not a tree either.
+
+    The time complexity for this is O(V+E) since DFS is used and that's the time complexity of DFS. This is because each vertex and edge will explored in
+    the worst case.
+    * */
+
+    static boolean [] visited = new boolean[1000];
+    static boolean isBipartite = true;
+    static ArrayList<Integer> path = new ArrayList<>();
+
+    public static void main(String[] args){
+        HashMap<Integer, ArrayList<Integer>> graph = new  HashMap<Integer, ArrayList<Integer>>();
+
+        ArrayList<Integer> adjacent1 = new ArrayList<Integer>();
+        adjacent1.add(3);adjacent1.add(4);graph.put(1,adjacent1);
+
+        ArrayList<Integer> adjacent2 = new ArrayList<Integer>();
+        adjacent2.add(1);adjacent2.add(3);graph.put(2,adjacent2);
+
+        ArrayList<Integer> adjacent3 = new ArrayList<Integer>();
+        adjacent3.add(4);graph.put(3,adjacent3);
+
+        ArrayList<Integer> adjacent4 = new ArrayList<Integer>();
+        adjacent4.add(1);adjacent4.add(2);graph.put(4,adjacent4);
+
+//         Iterating over the entry set of the map
+//        for (Map.Entry<Integer, ArrayList<Integer>> entry : graph.entrySet()) {
+//            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+//        }
+
+        DFS(graph);
+        BFS(graph);
+        printCycles(graph);
+
+
+
+    }
+
+    public static void DFS(HashMap<Integer, ArrayList<Integer>> graph){
+        visited = new boolean[1000];
+        System.out.print("DFS: ");
+        DFSHelper(graph,1);
+        System.out.println();
+
+    }
+
+    public static void DFSHelper(HashMap<Integer, ArrayList<Integer>> graph, int currentNode){
+        if(visited[currentNode])return;
+        System.out.print(currentNode+" ");
+        visited[currentNode]=true;
+        for(int x : graph.get(currentNode) ){
+            DFSHelper(graph,x);
+        }
+    }
+
+    public static void BFS(HashMap<Integer, ArrayList<Integer>> graph){
+        visited = new boolean[1000];
+        System.out.print("BFS: ");
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.add(1);
+        visited[1]=true;
+        while(!queue.isEmpty()){
+            int currentNode = queue.removeFirst();
+            System.out.print(currentNode+" ");
+            ArrayList<Integer> adjacent = graph.get(currentNode);
+            for(int x : adjacent) {
+                if (!visited[x]){
+                    queue.add(x);
+                    visited[x]=true;
+                }
+            }
+        }
+        System.out.println();
+
+    }
+
+    public static void printCycles(HashMap<Integer, ArrayList<Integer>> graph) {
+        visited = new boolean[1000];
+        int[] colors = new int[1000];
+        Arrays.fill(colors, -1);
+
+        for (int node : graph.keySet()) {
+            if (!visited[node]) {
+                colors[node] = 0;
+                if (!dfsCycle(graph, node, -1, colors)) {
+                    isBipartite = false;
+                }
+            }
+        }
+
+        System.out.println("Graph is " + (isBipartite ? "bipartite" : "not bipartite"));
+    }
+
+    private static boolean dfsCycle(HashMap<Integer, ArrayList<Integer>> graph, int node, int parent, int[] colors) {
+        visited[node] = true;
+        path.add(node);
+
+        for (int adj : graph.getOrDefault(node, new ArrayList<>())) {
+            if (!visited[adj]) {
+                colors[adj] = 1 - colors[node];
+                if (!dfsCycle(graph, adj, node, colors)) {
+                    return false;
+                }
+            } else if (adj != parent) {
+                // Cycle detected
+                int startIndex = path.indexOf(adj);
+                if (startIndex != -1) {
+                    int cycleLength = path.size() - startIndex;
+                    if (cycleLength % 2 == 1) {
+                        isBipartite = false;  // Odd-length cycle found
+                    }
+                    List<Integer> cycle = new ArrayList<>(path.subList(startIndex, path.size()));
+                    cycle.add(adj); // Adding the starting node at the end to complete the cycle
+                    System.out.println("Cycle: " + cycle);
+                }
+            }
+        }
+
+        path.remove(path.size() - 1);  // Backtracking: Remove the current node before returning
+        return true;
+    }
+
+
+}
+
+
